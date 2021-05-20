96e1912947f10152fba2bc2471f6b3a3685ec03d
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-05-21 00:03:24 +0600

    Update models.py

--- carts/models.py
+++ carts/models.py
@@ -3,56 +3,27 @@ from django.db import models
 from book_control.models import BookModel
 
 
-class Order(models.Model):
-    customer = models.ForeignKey(StudentProfileModel, on_delete=models.SET_NULL, null=True, blank=True)
+class OrderModel(models.Model):
+    student = models.ForeignKey(StudentProfileModel, on_delete=models.SET_NULL, null=True, blank=True)
+    book = models.ForeignKey(BookModel, null=True, blank=True, on_delete=models.SET_NULL)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    additional_note = models.TextField(null=True, blank=True)
+    is_approved = models.BooleanField(default=False)
+    is_canceled = models.BooleanField(default=False)
+    publisher_paid_approval = models.BooleanField(default=False)
+    student_paid_approval = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
     date_ordered = models.DateTimeField(auto_now_add=True)
-    completed = models.BooleanField(default=False, null=True, blank=True)
-    transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return str(self.id)
 
-    @property
-    def shipping(self):
-        shipping = False
-        order_items = self.orderitem_set.all()
-        for i in order_items:
-            if not i.product.digital:
-                shipping = True
-        return shipping
 
-    @property
-    def get_cart_total(self):
-        order_items = self.orderitem_set.all()
-        total = sum([item.get_total for item in order_items])
-        return total
-
-    @property
-    def get_cart_items(self):
-        order_items = self.orderitem_set.all()
-        total = sum([item.quantity for item in order_items])
-        return total
-
-
-class OrderItem(models.Model):
-    book = models.ForeignKey(BookModel, on_delete=models.SET_NULL, null=True, blank=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
-    quantity = models.IntegerField(default=0, null=True, blank=True)
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    @property
-    def get_total(self):
-        return self.product.price * self.quantity
-
-
-class ShippingAddress(models.Model):
-    customer = models.ForeignKey(StudentProfileModel, on_delete=models.SET_NULL, null=True, blank=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
-    address = models.CharField(max_length=200, null=True)
-    city = models.CharField(max_length=200, null=True)
-    state = models.CharField(max_length=200, null=True)
-    zipcode = models.CharField(max_length=200, null=True)
-    date_added = models.DateTimeField(auto_now_add=True)
+class OrderPaymentModel(models.Model):
+    order = models.ForeignKey(OrderModel, null=True, blank=True, on_delete=models.SET_NULL)
+    transaction_id = models.CharField(max_length=50, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    date_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.address
+        return str(self.id)
