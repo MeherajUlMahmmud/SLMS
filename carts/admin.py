42ca2c340c9f314d6338ae45c5ff8435543ca056
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-05-21 00:04:11 +0600

    Update admin.py

--- carts/admin.py
+++ carts/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
+admin.site.register(OrderModel)
+admin.site.register(OrderPaymentModel)
