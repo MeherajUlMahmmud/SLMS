f3ef2aacda3212abbcaeaa236aa95993a301e243
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-05-21 00:04:46 +0600

    Update forms.py

--- book_control/forms.py
+++ book_control/forms.py
@@ -7,8 +7,3 @@ class PostBookForm(forms.ModelForm):
     class Meta:
         model = BookModel
         fields = ('name', 'university', 'department', 'course', 'image', 'author', 'edition', 'description', 'price')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # self.fields['department'].queryset = DepartmentModel.objects.none()
-        # self.fields['course'].queryset = CourseModel.objects.none()
