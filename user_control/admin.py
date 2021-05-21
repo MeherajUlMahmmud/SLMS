980b5be36477519a44fa396e5e778971c0623a52
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-05-21 16:27:25 +0600

    Update admin.py

--- user_control/admin.py
+++ user_control/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from .models import *
 
+
+# class StudentProfileModelAdmin(admin.ModelAdmin):
+#     list_display = ('university', 'department')
+#     search_fields = ('university',)
+#     readonly_fields = ()
+#
+#     filter_horizontal = ()
+#     ordering = ()
+#     fieldsets = ()
+#     list_filter = ()
+#
+#
+# class PublisherProfileModelAdmin(admin.ModelAdmin):
+#     list_display = ('university', 'department')
+#     search_fields = ('university',)
+#     readonly_fields = ()
+#
+#     filter_horizontal = ()
+#     ordering = ()
+#     fieldsets = ()
+#     list_filter = ()
+
+
 admin.site.register(User)
 admin.site.register(StudentProfileModel)
 admin.site.register(PublisherProfileModel)
