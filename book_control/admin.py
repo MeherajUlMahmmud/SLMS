e176355310f7bb7ca28ddb50a485351be01ff11d
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-05-21 16:08:19 +0600

    Update admin.py

--- book_control/admin.py
+++ book_control/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 from .models import *
 
+
+class DepartmentModelAdmin(admin.ModelAdmin):
+    list_display = ('university', 'department')
+    search_fields = ('university',)
+    readonly_fields = ()
+
+    filter_horizontal = ()
+    ordering = ()
+    fieldsets = ()
+    list_filter = ()
+
+
+class CourseModelAdmin(admin.ModelAdmin):
+    list_display = ('university', 'department', 'course',)
+    search_fields = ('university',)
+    readonly_fields = ()
+
+    filter_horizontal = ()
+    ordering = ()
+    fieldsets = ()
+    list_filter = ()
+
+
 admin.site.register(UniversityModel)
-admin.site.register(DepartmentModel)
-admin.site.register(CourseModel)
+admin.site.register(DepartmentModel, DepartmentModelAdmin)
+admin.site.register(CourseModel, CourseModelAdmin)
 admin.site.register(BookModel)
